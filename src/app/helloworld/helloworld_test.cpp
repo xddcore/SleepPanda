73d68455672775292b72bbc6631e82776848f7c0
Chengsen Dong <1034029664@qq.com>
2023-01-25 20:58:37 +0000

    upload helloworld cpp unit test demo

---
+++ src/app/helloworld/helloworld_test.cpp
@@ -0,0 +1,50 @@
+/*
+ * @Author: Chengsen Dong 1034029664@qq.com
+ * @Date: 2023-01-25 20:43:04
+ * @LastEditors: Chengsen Dong 1034029664@qq.com
+ * @LastEditTime: 2023-01-25 20:46:02
+ * @FilePath: /SleepPanda/src/app/helloworld/helloworld_test.cpp
+ * @Description: 
+ * Copyright (c) 2023 by Chengsen Dong 1034029664@qq.com(www.github.com/xddcore), All Rights Reserved. 
+ */
+#include <iostream>
+#include <cppunit/TestRunner.h>
+#include <cppunit/TestResult.h>
+#include <cppunit/TestResultCollector.h>
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/BriefTestProgressListener.h>
+#include <cppunit/extensions/TestFactoryRegistry.h>
+
+class Test : public CPPUNIT_NS::TestCase
+{
+  CPPUNIT_TEST_SUITE(Test);
+  CPPUNIT_TEST(testHelloWorld);
+  CPPUNIT_TEST_SUITE_END();
+
+public:
+ void setUp(void) {}
+ void tearDown(void) {}
+protected:
+ void testHelloWorld(void) {
+ system("./helloworld >nul 2>nul");
+ }
+};
+CPPUNIT_TEST_SUITE_REGISTRATION(Test);
+
+int main()
+
+{
+  CPPUNIT_NS::TestResult controller;
+
+  CPPUNIT_NS::TestResultCollector result;
+  controller.addListener(&result);
+  
+  CPPUNIT_NS::BriefTestProgressListener progress;
+  controller.addListener(&progress);
+
+  CPPUNIT_NS::TestRunner runner;
+  runner.addTest(CPPUNIT_NS::TestFactoryRegistry::getRegistry().makeTest());
+  runner.run(controller);
+
+  return result.wasSuccessful() ? 0 : 1;
+}
