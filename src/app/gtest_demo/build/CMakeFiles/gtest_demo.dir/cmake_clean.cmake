e073520f248e833e01fe7a5b4defdfa69a062c9a
xddcore <1034029664@qq.com>
2023-01-27 09:46:21 +0000

    add gtest cmake build demo

---
+++ src/app/gtest_demo/build/CMakeFiles/gtest_demo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gtest_demo.dir/gtest_demo.cpp.o"
+  "CMakeFiles/gtest_demo.dir/gtest_demo.cpp.o.d"
+  "gtest_demo"
+  "gtest_demo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gtest_demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/app/gtest_demo/build/CMakeFiles/gtest_demo.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for gtest_demo.
+# This may be replaced when dependencies are built.
