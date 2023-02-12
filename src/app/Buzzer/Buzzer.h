d34c65e1cade41a7ec87a0e20e405e13bbee5fa5
Chengsen Dong <1034029664@qq.com>
2023-02-12 19:57:29 +0000

    Update Buzzer.h

--- src/app/Buzzer/Buzzer.h
+++ src/app/Buzzer/Buzzer.h
@@ -2,7 +2,7 @@
  * @Author: Chengsen Dong 1034029664@qq.com
  * @Date: 2023-02-12 12:50:52
  * @LastEditors: Chengsen Dong 1034029664@qq.com
- * @LastEditTime: 2023-02-12 19:54:25
+ * @LastEditTime: 2023-02-12 19:57:25
  * @FilePath: /SleepPanda/src/app/Buzzer/Buzzer.h
  * @Description: 
  * Copyright (c) 2023 by ${git_name_email}(www.github.com/xddcore), All Rights Reserved. 
@@ -16,7 +16,7 @@ struct Buzzer_Settings {
     unsigned int Buzzer_GPIO_Pin = 6;
     unsigned int Buzzer_On_Level = 0; //蜂鸣器低电平触发
     unsigned int Buzzer_Off_Level = 1; //蜂鸣器高电平不触发
-}
+};
 
 #define Buzzer_On 1
 #define Buzzer_Off 0
