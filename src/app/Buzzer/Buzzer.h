16769d975cfc30e2df8b0c54d0e7921e6d3d5adf
Chengsen Dong <1034029664@qq.com>
2023-02-12 20:05:10 +0000

    Update Buzzer.h

--- src/app/Buzzer/Buzzer.h
+++ src/app/Buzzer/Buzzer.h
@@ -2,7 +2,7 @@
  * @Author: Chengsen Dong 1034029664@qq.com
  * @Date: 2023-02-12 12:50:52
  * @LastEditors: Chengsen Dong 1034029664@qq.com
- * @LastEditTime: 2023-02-12 20:03:59
+ * @LastEditTime: 2023-02-12 20:05:05
  * @FilePath: /SleepPanda/src/app/Buzzer/Buzzer.h
  * @Description: 
  * Copyright (c) 2023 by ${git_name_email}(www.github.com/xddcore), All Rights Reserved. 
@@ -25,7 +25,7 @@ class Buzzer{
     public:
     Buzzer(Buzzer_Settings My_Buzzer_Settings);
     ~Buzzer();
-    int Buzzer_Contorl(unsigned int Buzzer_OnOff)
+    int Buzzer_Contorl(unsigned int Buzzer_OnOff);
     
     private:
     Buzzer_Settings My_Buzzer_Settings;
