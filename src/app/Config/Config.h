23aeff8bbb365df93f8c2fe0aa18f3eb5fd93570
Chengsen Dong <1034029664@qq.com>
2023-02-12 19:24:36 +0000

    upload Config.h

---
+++ src/app/Config/Config.h
@@ -0,0 +1,21 @@
+/*
+ * @Author: Chengsen Dong 1034029664@qq.com
+ * @Date: 2023-02-12 12:33:14
+ * @LastEditors: Chengsen Dong 1034029664@qq.com
+ * @LastEditTime: 2023-02-12 19:21:24
+ * @FilePath: /SleepPanda/src/app/config/config.h
+ * @Description: 
+ * Copyright (c) 2023 by ${git_name_email}(www.github.com/xddcore), All Rights Reserved. 
+ */
+
+//规定:所有函数成功则返回0，失败则返回-1
+//
+
+#ifndef SRC_CONFIG_H
+#define SRC_CONFIG_H
+
+// RPI_DEBUG == 1: RPI 4b Run code
+// RPI_DEBUG == 0 : Any computer Run code(test code logic)
+#define RPI_DEBUG 1
+
+#endif //SRC_CONFIG_H
